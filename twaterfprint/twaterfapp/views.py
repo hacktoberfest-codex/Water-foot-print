64279a7340f4abd95a8ca2bd1e0496e9c4a942cb
Katsu <messyaikei456@gmail.com>
2023-09-16 22:49:53 +0530

    back

--- twaterfprint/twaterfapp/views.py
+++ twaterfprint/twaterfapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+def index(request):
+    return render(request,'index.html')
+
+def page1(request):
+    return render(request,'page1.html')
+def resultpage(request):
+    return render(request,'result.html')
+
+
 # Create your views here.
